123e719ce8f8923cce3a71274f1acb18c5b33b27
Harikaummidi <118156328+Harikaummidi@users.noreply.github.com>
2022-11-23 11:44:50 +0530

    main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("%d \n",2);
+    printf("%f \n",0.1);
+    printf("%ld \n"),5456435213545;
+    printf("%lf \n"),0.21654546;
+    printf(" %X \n "),16;
+    printf(" %C \n "),'h';
+    printf(" %S \n ")," im harika ";
+
+
+
+    puts(" this is haria ");
+
+    return 0;
+}
